131ec532d80125ae3545d4a18eefe37c80d906ed
zakirzhan <zako1996@mail.ru>
2020-05-16 01:36:21 +0200

    updated youtube_crawler \c
    /c
    ΩΩz©zΩ≈≈x
    
    ;
    git
    ]
    [
    adasda

--- Classes/YoutubeCrawler.class.php
+++ Classes/YoutubeCrawler.class.php
@@ -5,7 +5,7 @@ class YoutubeCrawler
 
 	public static function search($query){
 
-		$url = 'https://api.bulbul.su/api.php?query='.urlencode($query).'&method=search&iaadminept=on';
+		$url = $set['api']['base_url'].'?query='.urlencode($query).'&method=search&iaadminept=on';
 	
 		$json = get_web_page($url);
 

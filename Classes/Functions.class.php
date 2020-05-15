b88f37698199490da30292cf10278826ab1e6c4e
zakirzhan <zako1996@mail.ru>
2020-05-16 01:18:31 +0200

    Updated: added autoloader functions, created core.php

---
+++ Classes/Functions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class Functions {
+	/**
+     * __call
+     *
+     * @param string	$name
+     * @param string OR array	$key
+     * @return string OR array
+     */
+    public function __call($name, $key) {
+     	require_once(ROOT . '/Functions/' . $name . '.php');
+     	return call_user_func_array($name, $key);
+    }
+}
+?>
---
+++ core.php
@@ -0,0 +1,48 @@
+<?php 
+
+require_once(ROOT."Classes/Functions.class.php");
+
+require_once(ROOT."Classes/MyRedis.class.php");
+$redis = new MyRedis;
+
+require_once(ROOT."Classes/YoutubeCrawler.class.php");
+/**
+* Системные константы
+*/
+define ('MT', microtime(TRUE));
+define ('TIME', time());
+define ('ACTION', isset($_GET['action']) ? $_GET['action'] : '');
+define ('ROOT', str_replace('system/core', '', str_replace(DIRECTORY_SEPARATOR, '/', dirname(__FILE__))));
+
+/**
+* Основные конфигурации системы
+*/
+ini_set ('error_reporting', -1);  								// Включаем полное отображение ошибок
+ini_set ('xhtml_errors', TRUE);   								// Включаем полное отображение ошибок xHTML разметки
+ini_set ('display_errors', FALSE); 								// Включаем вывод ошибок на экран
+ini_set ('ignore_repeated_errors', TRUE);       				// Выключаем повторый показ ошибок
+ini_set ('session.use_trans_sid', FALSE);       				// Выключаем подстановку PHPSESSID в ссылки
+ini_set ('magic_quotes_gpc', FALSE);                            // Выключаем экранирование кавычек
+ini_set ('magic_quotes_runtime', FALSE);                        // Выключаем экранирование кавычек
+ini_set ('magic_quotes_sybase', FALSE);                         // Выключаем экранирование кавычек
+ini_set ('register_globals', FALSE);                            // Выключаем глобальные переменные
+ini_set ('arg_separator.output', '&amp;');      				// Включаем переобразование & в &amp;
+
+mb_internal_encoding ('UTF-8'); 								// Устанавливаем кодировку UTF-8
+date_default_timezone_set ('Europe/Moscow'); 		// Устанавливаем временную зону
+setlocale(LC_ALL, 'ru_RU.utf-8');								// Устанавливаем локализацию
+
+
+/**
+* Автозагрузка классов
+*/
+function __autoload($name) {
+	
+	if(file_exists(ROOT . 'Functions/' . $name . '.php')) {
+		require_once ROOT . 'Functions/' . $name . '.php';
+	}
+}
+
+$function = new Functions;
+ 
+ ?>

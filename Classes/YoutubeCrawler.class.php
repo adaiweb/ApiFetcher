88a4a2ec7d76f7db7858cacd16ce813e32160e0e
zakirzhan <zako1996@mail.ru>
2020-05-16 02:18:01 +0200

    updated

--- Classes/YoutubeCrawler.class.php
+++ Classes/YoutubeCrawler.class.php
@@ -1,17 +1,33 @@
 <?php 
 
 class YoutubeCrawler
-{
+{	
+
 
 	public static function search($query){
 
 		$url = $set['api']['base_url'].'?query='.urlencode($query).'&method=search&iaadminept=on';
 	
-		$json = get_web_page($url);
+		$json = self::curl_get_contents($url);
 
 		return $json;
 
 	}
+	
+
+	private static function curl_get_contents($url) {
+		$ch = curl_init($url);
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11) AppleWebKit/601.1.56 (KHTML, like Gecko) Version/9.0 Safari/601.1.56');
+
+		$data = curl_exec($ch);
+		curl_close($ch);
+		
+		return $data;
+	}
+	
 
 }
 

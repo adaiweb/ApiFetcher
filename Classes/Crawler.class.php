d9a62b3620d2cd7ac0936745cd20dbf41576f6d6
zakirzhan <zako1996@mail.ru>
2020-08-17 20:14:33 +0200

    fixed bug with Crawler

--- Classes/Crawler.class.php
+++ Classes/Crawler.class.php
@@ -3,18 +3,33 @@
 class Crawler
 {	
 
+	private function curl_get_contents($url)
+	{
+	  $uagent = "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.14";
 
-	public static function search($query){
+	  $ch = curl_init( $url );
 
-		$url = API_BASE_URL.'?query='.urlencode($query).'&method=search&iaadminept=on';
-	
-		$json = self::curl_get_contents($url);
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   // возвращает веб-страницу
+	  curl_setopt($ch, CURLOPT_HEADER, 0);           // не возвращает заголовки
+	  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);   // переходит по редиректам
+	  curl_setopt($ch, CURLOPT_ENCODING, "");        // обрабатывает все кодировки
+	  curl_setopt($ch, CURLOPT_USERAGENT, $uagent);  // useragent
+	  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120); // таймаут соединения
+	  curl_setopt($ch, CURLOPT_TIMEOUT, 120);        // таймаут ответа
+	  curl_setopt($ch, CURLOPT_MAXREDIRS, 10);       // останавливаться после 10-ого редиректа 
 
-		return $json;
+	  $content = curl_exec( $ch );
+	  $err     = curl_errno( $ch );
+	  $errmsg  = curl_error( $ch );
+	  $header  = curl_getinfo( $ch );
+	  curl_close( $ch );
 
-	}
-
-	
+	  $header['errno']   = $err;
+	  $header['errmsg']  = $errmsg;
+	  $header['content'] = $content;
+	  return $content;
+	} 
+	 
 
 }
 

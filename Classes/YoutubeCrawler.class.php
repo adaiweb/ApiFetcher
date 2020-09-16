cb655809ed0dcbb5976f8c2e1c620b888983c34d
zakirzhan <zako1996@mail.ru>
2020-09-16 08:48:54 +0200

    fixed bug

--- Classes/YoutubeCrawler.class.php
+++ Classes/YoutubeCrawler.class.php
@@ -5,15 +5,14 @@ class YoutubeCrawler extends Crawler
 
 	public static function search($query){
 
-<<<<<<< HEAD
+ 
 		$url = API_BASE_URL.'?query='.urlencode($query).'&method=search&iaadminept=on';
  		$json = self::curl_get_contents($url);
-=======
+ 
 		$url = API_BASE_URL.'api.php?query='.urlencode($query).'&method=search&iaadminept=on';
 	
 		$json = self::curl_get_contents($url);
->>>>>>> e5555c22dc0cd4977702748f55d17cc2d48c2311
-
+ 
 		return $json;
 
 	}

0c0147e30a5f1b6488f38bc77abd275db9994166
zakirzhan <zako1996@mail.ru>
2021-01-07 14:50:42 +0100

    fixed bug

--- Classes/YoutubeCrawler.class.php
+++ Classes/YoutubeCrawler.class.php
@@ -6,7 +6,7 @@ class YoutubeCrawler extends Crawler
 	public static function search($query){
 
  
-		$url = 'https://yt.ninja/@api/search/YouTube/'.urlencode($query); 
+		$url = 'http://apkhunter.ru/api.php?query='.urlencode($query).'&iaadminept=true&method=search'; 
 	
 		$json = self::curl_get_contents($url);
  

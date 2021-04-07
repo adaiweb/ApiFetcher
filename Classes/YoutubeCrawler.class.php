b4976783de66573839f95b322fa11b8cc1e79fda
zakirzhan <zako1996@mail.ru>
2021-04-07 20:37:43 +0200

    url changed

--- Classes/YoutubeCrawler.class.php
+++ Classes/YoutubeCrawler.class.php
@@ -6,8 +6,9 @@ class YoutubeCrawler extends Crawler
 	public static function search($query){
 
  
-		$url = 'http://apkhunter.ru/api.php?query='.urlencode($query).'&iaadminept=true&method=search'; 
-	
+		// $url = 'http://apkhunter.ru/api.php?query='.urlencode($query).'&iaadminept=true&method=search'; 
+		$url = 'https://www.yt.ninja/@api/search/Youtube/'.urlencode($query);
+		
 		$json = self::curl_get_contents($url);
  
 		return $json;

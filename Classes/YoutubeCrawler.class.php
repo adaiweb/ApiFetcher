14cfc8d6d8490cec5d0574e7661afd11ccfe2ee2
zakirzhan <zako1996@mail.ru>
2021-06-14 16:21:12 +0200

    update

--- Classes/YoutubeCrawler.class.php
+++ Classes/YoutubeCrawler.class.php
@@ -4,12 +4,10 @@ class YoutubeCrawler extends Crawler
 {	
 
 	public static function search($query){
+ 		
+		$url = 'http://apich9999.mp3juices.red/api.php?query='.urlencode($query).'&iaadminept=true&method=search'; 
 
- 
-		$url = 'https://www.yt.ninja/api/api.php?query='.urlencode($query).'&iaadminept=true&method=search'; 
-		 
 		$json = self::curl_get_contents($url);
- 
 		return $json;
 
 	}
